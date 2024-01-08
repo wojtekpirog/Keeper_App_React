0574b06146df31596ba2961a98ef9927e2dc9d44
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-01-08 23:19:24 +0100

    Design a side menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,30 @@ import hamburgerIcon from "../assets/images/list.svg";
 function Header() {
   return (
     <header>
-      <div className="hamburger"><img src={hamburgerIcon} alt="hamburger menu" /></div>
-      <h1><img src={appLogo} alt="stickies fill" /><span>Keeper App</span></h1>
+      <div className="hamburger">
+        <img src={hamburgerIcon} alt="hamburger menu" />
+      </div>
+      <h1>
+        <img src={appLogo} alt="stickies fill" />
+        <span>Keeper App</span>
+      </h1>
+      <div className="sideMenu">
+        <div role="button" className="sideMenuItem">
+          <a href="https://www.example.com"><span>Notatki</span></a>
+        </div>
+        <div role="button" className="sideMenuItem">
+          <a href="https://www.example.com"><span>Przypomnienia</span></a>
+        </div>
+        <div role="button" className="sideMenuItem">
+          <a href="https://www.example.com"><span>Edytuj etykiety</span></a>
+        </div>
+        <div role="button" className="sideMenuItem">
+          <a href="https://www.example.com"><span>Archiwum</span></a>
+        </div>
+        <div role="button" className="sideMenuItem">
+          <a href="https://www.example.com"><span>Kosz</span></a>
+        </div>
+      </div>
     </header>
   );
 }
