5ea9d5aa611bbe192a8768f1ac12c6cd224f00d0
wojtekpirog <114958949+wojtekpirog@users.noreply.github.com>
2024-02-17 20:13:33 +0100

    Refactor code to render notes based on note data inside the app's database

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note() {
-  return(
+function Note({title, content}) {
+  return (
     <div className="note">
-      <h1>Title</h1>
-      <p>Content...</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
     </div>
   );
 }
--- src/components/NoteContainer.jsx
+++ src/components/NoteContainer.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Note from "./Note";
+import notes from "../assets/data/notes";
 
 function NoteContainer() {
+  // const [notes, setNotes] = useState([]);
+
   return (
-    <div className="noteContainer">
-      <Note />
-    </div>
+    <main>
+      <div className="noteContainer">
+        {notes.map((note, index) => <Note key={index} id={index} title={note.title} content={note.content} />)}
+      </div>      
+    </main>
   );
 }
 
